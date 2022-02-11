3daa344b14aa150faa64a42a12fad7e1ea08f195
Pavel-Baturin <pavel.baturin.92@gmail.com>
2022-02-11 18:06:13 +0200

    Добавляет форму для книги контактов

--- src/App.js
+++ src/App.js
@@ -1,10 +1,41 @@
+import React, { Component } from "react";
+import ContactForm from "./components/ContactForm/ContactForm";
+// import Filter from "./components/Filter/Filter";
+import ContactList from "./components/ContactList/ContactList";
 
-function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
+class App extends Component {
+  state = {
+    contacts: [],
+    filter: "",
+  };
+
+  deleteContact = (contactId) => {
+    this.setState((prevState) => ({
+      contacts: prevState.contacts.filter(
+        (contact) => contact.id !== contactId
+      ),
+    }));
+  };
+
+  submitContactForm = (contact) => {
+    const listContacts = this.state.contacts;
+    listContacts.push(contact);
+  };
+
+  render() {
+    return (
+      <>
+        <h1>Phonebook</h1>
+        <ContactForm onSubmit={this.submitContactForm} />
+        <h2>Contacts</h2>
+        {/* <Filter /> */}
+        <ContactList
+          contacts={this.state.contacts}
+          onDeleteContact={this.deleteContact}
+        />
+      </>
+    );
+  }
 }
 
 export default App;
